483a7057a31876a8ef4a97d0aeb2bceeddcd51b2
thinkabyaroo <thinkabyaroo2911@gmail.com>
2026-03-05 12:55:25 +0630

    Fix: Modify the attendance

--- app/crud/attendance.py
+++ app/crud/attendance.py
@@ -1,8 +1,9 @@
 from sqlalchemy.orm import Session
 from app.models.attendance import Attendance
-from app.schemas.attendance import AttendanceCreate
+from app.schemas.attendance import AttendanceCreate, AttendanceUpdate
 
 
+# Create
 def create_attendance(db: Session, attendance: AttendanceCreate):
     db_obj = Attendance(**attendance.model_dump())
     db.add(db_obj)
@@ -11,5 +12,36 @@ def create_attendance(db: Session, attendance: AttendanceCreate):
     return db_obj
 
 
-def get_all_attendance(db: Session):
-    return db.query(Attendance).all()
+# Get All
+def get_attendances(db: Session, skip: int=0, limit: int=100):
+    return db.query(Attendance).offset(skip).limit(limit).all()
+
+
+# Get by ID
+def get_attendance(db: Session, attendance_id: int):
+    return db.query(Attendance).filter(Attendance.id == attendance_id).first()
+
+
+# Update
+def update_attendance(db: Session, attendance_id: int, attendance: AttendanceUpdate):
+    db_attendance = get_attendance(db, attendance_id)
+    if not db_attendance:
+        return None
+    
+    for key, value in attendance.model_dump(exclude_unset=True).items():
+        setattr(db_attendance, key, value)
+
+    db.commit()
+    db.refresh(db_attendance)
+    return db_attendance
+
+
+# Delete
+def delete_attendance(db: Session, attendance_id: int):
+    db_attendance = get_attendance(db, attendance_id)
+    if not db_attendance:
+        return None
+    
+    db.delete(db_attendance)
+    db.commit()
+    return db_attendance
